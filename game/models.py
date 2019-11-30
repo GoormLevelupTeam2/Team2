77d7dbfd3aa9a60faa820504f27aff72df8d8dd3
congbro <2044smile@naver.com>
2019-11-30 17:55:39 +0900

    models Field question, answer, correct ADD

--- game/models.py
+++ game/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    question = models.CharField( # 문제출력
+        max_length=128
+    )
+    answer = models.CharField( # 정답입력
+        max_length=128,
+        blank=True,
+        null=True
+    )
+    correct = models.BooleanField(
+        default=False # False 오답, True 정답
+    )
+
+    def __str__(self):
+        return '질문 : ' + self.question
